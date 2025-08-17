d68319c5ad76c601b7f560137303b5ccbf4fa4a8
Jay <jaygondaliya2309@gmail.com>
2025-08-17 07:05:24 +0530

    Standarized error and response from server by adding utility classes

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400; // anything above 400 is client or server error
+  }
+}
+
+export { ApiResponse };
