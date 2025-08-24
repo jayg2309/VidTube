a860baa0fc8debe18f28574904c4fd832671e4f0
Jay <jaygondaliya2309@gmail.com>
2025-08-24 09:41:22 +0530

    Add JWT authentication and password encryption to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -11,6 +11,8 @@ updatedAt Date
 */
 
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -58,5 +60,48 @@ const userSchema = new Schema(
   },
   { timestamps: true } // automatically creates createdAt and updatedAt Date fields
 );
+
+//encrypting password using bcrypt - middleware
+//just before saving the pw - encrypts it
+
+userSchema.pre("save", async function (next) {
+  //only run if password is modified
+  if (!this.modified("password")) return next();
+  //encrypting
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+// match to see if correct
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password); // true or false
+};
+
+//generating access token
+userSchema.methods.generateAccessToken = function () {
+  //short lived access token
+  return jwt.sign(
+    {
+      _id: this._id, //payload
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET, //key
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY } //expiry
+  );
+};
+
+//generate refresh token - not short lived
+userSchema.methods.generateRefreshToken = function () {
+  //refresh token only has id to update the fields
+  return jwt.sign(
+    {
+      _id: this._id, //payload
+    },
+    process.env.REFRESH_TOKEN_SECRET, //key
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY } //expiry
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
