d368914e6b0b8d2fadf2224b14d27bc62c65a168
Jay <jaygondaliya2309@gmail.com>
2025-08-24 10:52:55 +0530

    Add Cloudinary integration and middleware for file uploads - Added cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors"; //--> who can talk to DB
-// import { use } from "react";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(
 app.use(express.json({ limit: "16kb" })); // all the data which comes in
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // serve images, css directly
-
+app.use(cookieParser());
 //import routes
 import healthcheckRouter from "./routes/healthchecks.routes.js";
 
--- src/routes/healthchecks.routes.js
+++ src/routes/healthchecks.routes.js
@@ -5,6 +5,7 @@ import { healthcheck } from "../controllers/healthcheck.controllers.js";
 const router = Router();
 
 router.route("/").get(healthcheck);
+
 ///api/v1/healthcheck/test
 // router.route("/test").get(healthcheck);
 
