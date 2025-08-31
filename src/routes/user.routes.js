e724578970ee021f39f5385cb2d2b7f0a0b9382a
Jay <jaygondaliya2309@gmail.com>
2025-08-31 18:12:17 +0530

    Added all user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/usercontroller.js";
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getWatchHistory,
+} from "../controllers/usercontroller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
+//unsecure routes - no need to verifyJWT
 //serve on /register
 router.route("/register").post(
   //getting multiple fields - cover image,avatar
@@ -21,9 +34,24 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
 //secured routes
 //req first goes through verifyJWT(middleware) then goes to logoutUser controller
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/history").get(verifyJWT, getWatchHistory);
+//images wadu
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 // Temporary test endpoint to verify multer is working
 router.post("/multer-test", upload.single("avatar"), (req, res) => {
